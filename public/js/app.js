70ed223f73bb5a01607299cec423739b602fd59f
UTSean <yutongsean@qq.com>
2017-11-13 21:10:10 +0000

    Finish user auth

--- public/js/app.js
+++ public/js/app.js
@@ -42796,7 +42796,7 @@ if (false) {
 /* 42 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n    font-family: Hiragino Sans GB, \"Hiragino Sans GB\". Helvetica. \"Microsoft YaHei\", Arial,sans-serif;\n                                                    ^\n      Invalid CSS after '...ragino Sans GB\"': expected expression (e.g. 1px, bold), was '. Helvetica. \"Micro'\n      in /home/vagrant/Code/larabbs/resources/assets/sass/app.scss (line 14, column 54)\n    at runLoaders (/home/vagrant/Code/larabbs/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /home/vagrant/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/vagrant/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/vagrant/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/vagrant/Code/larabbs/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/home/vagrant/Code/larabbs/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/home/vagrant/Code/larabbs/node_modules/async/dist/async.js:958:16)\n    at options.error (/home/vagrant/Code/larabbs/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,9 @@
-<footer class="foooter">
+<footer class="footer">
     <div class="container">
         <p class="pull-left">
-            From <a href="https://github.com/UTSean" target="_black">UTSean</a>
-            <span style="color:#e27575;font-size: 14px;">❤</span>
-
-            <p class="pull-left"><a href="mailto:yutongsean@qq.com">Contact me</p>
+            By <a href="https://github.com/UTSean" target="_blank">UTSean</a> Coding <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
+
+        <p class="pull-right"><a href="mailto:yutongsean@gmail.com">Contact me</a></p>
     </div>
+</footer>
