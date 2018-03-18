1e4d24d1b15c8adb8feeeb2ea6d22c479946126c
UTSean <yutongsean@qq.com>
2018-03-18 20:43:00 +0000

    Seeding topics and users data

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
-    protected $fillable = [
-        'name', 'description',
-    ];
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 }
