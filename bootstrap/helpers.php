ca18bfcc6a1294d99943fe776c5344dc8b82e03a
UTSean <yutongsean@qq.com>
2018-03-20 15:54:18 +0000

    WYSIWYG Editor

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,9 +5,9 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
 }
 
-function make_excerpt()
+function make_excerpt($value, $length = 200)
 {
-    $excerpt = trim(preg_replace('/\r\n|\r|\n+',' ', strip_tags($value)));
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/',' ', strip_tags($value)));
     return str_limit($excerpt, $length);
 }
 ?>
