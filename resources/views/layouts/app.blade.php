d4a6d53ffb8e4277851068831b6462011410a3b2
UTSean <yutongsean@qq.com>
2018-03-24 09:13:34 +0000

    Integrated administrator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,13 @@
 
         </div>
 
+        @if (app()->isLocal())
+            @include('sudosu::user-selector')
+        @endif
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
         @yield('scripts')
+
+
     </body>
 </html>
