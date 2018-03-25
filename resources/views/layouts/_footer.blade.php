29dd213292221db7ba290aec1e18ab23cd0235f4
UTSean <yutongsean@qq.com>
2018-03-25 07:40:14 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
         <p class="pull-left">
             Coding from <a href="https://github.com/UTSean" target="_blank">UT.Shaw </a> <span style="color: #e27575;font-size: 14px;">❤</span>
 </p>
-        <p class="pull-right"><a href="mailto:yutongsean@gmail.com">Connet me</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">Communication</a></p>
     </div>
 </footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title', 'LaraBBS') - Ein Fortschritt in Lavavel</title>
-        <meta name="description" content="@yield('description', 'LaraBBS Forum')" />
+        <title>@yield('title', 'LaraBBS') - {{ setting('site_name', 'Practice with Laravel')}}</title>
+        <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS forum')" />
+        <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS', 'LaraBBS'))">
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         @yield('styles')
